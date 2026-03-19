a975e8c74348408d33d3b44b0a44f7d46252319f
VietDev181 <daohuyvietapp@gmail.com>
2026-03-19 14:56:32 +0700

    Update XML Splash and Log Main Activity

---
+++ app/src/main/java/com/example/modernshop/activities/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.modernshop.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.modernshop.MainActivity
+import com.example.modernshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var  binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
